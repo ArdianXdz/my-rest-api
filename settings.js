e465cd51964a50777851ee7a707faa75e142af34
ArdianXdz <154233991+ArdianXdz@users.noreply.github.com>
2023-12-26 04:34:59 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ global.creator = 'ArdianOfc'// yourname
 global.MONGO_DB_URI = "mongodb+srv://vinaabotzmd:dians2210@cluster0.kodf1ls.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "xyrobototp@gmail.com" //email
-global.email_password = "ardian" //application password email
+global.email_password = "uyvk snbw rqka jrqt" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
